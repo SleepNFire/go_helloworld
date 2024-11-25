58223422158c563b138d47cba80a780a8f4035b5
Alexandre Létocart <letocart.alex@gmail.com>
2024-11-25 09:37:04 +0100

    escape game with better indication

--- internal/helloworld/api.go
+++ internal/helloworld/api.go
@@ -11,8 +11,16 @@ func InitHelloEndpoint() (*HelloEndpoint, error) {
 
 func (he *HelloEndpoint) RegisterEndpoint(router *gin.Engine) {
 	router.GET("/turing", he.HelloWorld)
+	router.GET("/alan-turing", he.HelloWorld)
+	router.GET("/AlanTuring", he.HelloWorld)
+	router.GET("/alanturing", he.HelloWorld)
+	router.GET("/reponse", he.NotResponse)
 }
 
 func (he *HelloEndpoint) HelloWorld(c *gin.Context) {
 	c.JSON(200, "Félicitations, vous avez terminé l'initiation ! J'espère que le jeu de piste vous a plu.")
+}
+
+func (he *HelloEndpoint) NotResponse(c *gin.Context) {
+	c.JSON(200, "Il faut changer le mot reponse par le bon nom")
 }
